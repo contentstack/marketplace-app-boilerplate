cea331c9426883eceed3d8c50cdd8382b6d287b3
abhishek305 <abhishekshaji1994@gmail.com>
2022-08-18 18:45:19 +0530

    feat:entry & dashboard component

--- src/routes/EntrySidebar.tsx
+++ src/routes/EntrySidebar.tsx
@@ -1,9 +1,24 @@
+import Icon from '../images/sidebarwidget.svg';
+import { Link } from 'react-router-dom';
+
 const EntrySidebarExtension = () => {
   return (
-    <div className='entry-sidebar-extension'>
-      <label>Basic App Template field</label>
-      <p>Display Entry Sidebar Extension</p>
+    <div className='entry-sidebar'>
+    <div className='entry-sidebar-container'>
+      <div className='entry-sidebar-icon'>
+        <img src={Icon} alt='icon' />
+      </div>
+      <div className='entry-sidebar-content'>
+        <h4>Sidebar Widget</h4>
+        <p>
+          This is the iframe that contains your dashboard widget
+          <br />
+          Build you app now
+        </p>
+        <Link to='/'>Learn more</Link>
+      </div>
     </div>
+  </div>
   );
 };
 
--- src/routes/StackDashboard.tsx
+++ src/routes/StackDashboard.tsx
@@ -1,7 +1,23 @@
+import Icon from '../images/Icon.svg';
+import { Link } from 'react-router-dom';
+
 const StackDashboardExtension = () => {
   return (
     <div className='dashboard'>
-      <label>Dashboard App Example</label>
+      <div className='dashboard-container'>
+        <div className='dashboard-icon'>
+          <img src={Icon} alt='icon' />
+        </div>
+        <div className='dashboard-content'>
+          <h4>Dashboard Widget</h4>
+          <p>
+            This is the iframe that contains your dashboard widget
+            <br />
+            Build you app now
+          </p>
+          <Link to='/'>Learn more</Link>
+        </div>
+      </div>
     </div>
   );
 };
