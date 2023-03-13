e748d2b2db68ea80be81b4dad1139716c48cadd4
Shrutika Almeida <shrutika.almeida@Shrutikas-MacBook-Pro.local>
2023-03-13 17:30:43 +0530

    fix:ECO-2346 resolve snyk issues

--- src/common/hooks/useAppSdk.tsx
+++ src/common/hooks/useAppSdk.tsx
@@ -8,6 +8,10 @@ import { useContext } from "react";
 /**
  * Getter and setter for appSdk instance.
  * To be used during Sdk initialisation
+ * @returns appSdk;
+ *
+ * Eg:
+ * const appSdk = useAppSdk();
  */
 export const useAppSdk = () => {
   const { appSdk } = useContext(MarketplaceAppContext) as MarketplaceAppContextType;
