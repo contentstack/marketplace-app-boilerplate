7c3d1a8b0caa9433a88bc071c04d039e8aad53fe
Contentstack-AnkitaD <ankita.dodamani@contentstack.com>
2023-07-11 17:18:11 +0530

    fix#6: "Show Json Modal" CTA & Json modal of Configured values on all Locations beside the Displayed Value.

--- src/containers/DashboardWidget/StackDashboard.tsx
+++ src/containers/DashboardWidget/StackDashboard.tsx
@@ -1,12 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Icon from "../../assets/Icon.svg";
 import localeTexts from "../../common/locales/en-us/index";
 import parse from "html-react-parser";
 import { useAppConfig } from "../../common/hooks/useAppConfig";
+import RawConfigButton from "../../components/ViewRawConfig/RawConfigButton";
+import RawConfigModal from "../../components/ViewRawConfig/RawConfigModal";
 
 const StackDashboardExtension = () => {
   const appConfig = useAppConfig();
   console.log("appConfig", appConfig);
+
+  const [isRawConfigModalOpen, setRawConfigModalOpen] = useState(false);
+
+  const handleViewRawConfig = () => {
+    setRawConfigModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setRawConfigModalOpen(false);
+  };
 
   useEffect(() => {
     console.log("appConfig:", appConfig);
@@ -23,9 +35,11 @@ const StackDashboardExtension = () => {
         <div className="app-component-content">
           <h4>{localeTexts.DashboardWidget.title}</h4>
           <p className="configValues">
-            <b>Username: </b>
-            {name}
+              <b>Username: </b>
+              {name}
+            <RawConfigButton onClick={handleViewRawConfig} />
           </p>
+          {isRawConfigModalOpen && <RawConfigModal config={appConfig!} onClose={handleCloseModal} />}
           <p>{parse(localeTexts.DashboardWidget.body)}</p>
           <a target="_blank" rel="noreferrer" href={localeTexts.DashboardWidget.button.url}>
             {localeTexts.DashboardWidget.button.text}
