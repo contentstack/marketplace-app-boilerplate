57f8d9814fa14270b110eacf28ea45f58d23bfa7
Contentstack-AnkitaD <ankita.dodamani@contentstack.com>
2023-07-11 16:23:25 +0530

    fix#5:Display the Value saved in AppConfig on all Locations [Currently Configured for Stackboard]

--- src/containers/DashboardWidget/StackDashboard.tsx
+++ src/containers/DashboardWidget/StackDashboard.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import Icon from "../../assets/Icon.svg";
 import localeTexts from "../../common/locales/en-us/index";
 import parse from "html-react-parser";
+import { useAppConfig } from "../../common/hooks/useAppConfig";
 
 const StackDashboardExtension = () => {
+  const appConfig = useAppConfig();
+  console.log("appConfig", appConfig);
+
+  useEffect(() => {
+    console.log("appConfig:", appConfig);
+  }, [appConfig]);
+
+  const name = appConfig?.username || "";
+
   return (
     <div className="dashboard">
       <div className="dashboard-container">
@@ -11,11 +22,12 @@ const StackDashboardExtension = () => {
         </div>
         <div className="app-component-content">
           <h4>{localeTexts.DashboardWidget.title}</h4>
+          <p className="configValues">
+            <b>Username: </b>
+            {name}
+          </p>
           <p>{parse(localeTexts.DashboardWidget.body)}</p>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={localeTexts.DashboardWidget.button.url}>
+          <a target="_blank" rel="noreferrer" href={localeTexts.DashboardWidget.button.url}>
             {localeTexts.DashboardWidget.button.text}
           </a>
         </div>
