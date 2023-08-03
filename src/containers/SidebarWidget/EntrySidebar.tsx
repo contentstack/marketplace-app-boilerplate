7d343843061c0ffe5290e256de8873f43c7a92cb
Contentstack-AnkitaD <ankita.dodamani@contentstack.com>
2023-08-03 14:46:08 +0530

    feat(UI Screen): Implemented UI for Entry Sidebar Location.

--- src/containers/SidebarWidget/EntrySidebar.tsx
+++ src/containers/SidebarWidget/EntrySidebar.tsx
@@ -1,23 +1,61 @@
-import Icon from "../../assets/sidebarwidget.svg";
+import { useState } from "react";
 import localeTexts from "../../common/locales/en-us/index";
 import parse from "html-react-parser";
+import { useAppConfig } from "../../common/hooks/useAppConfig";
+import "./EntrySidebar.css";
+import Icon from "../../assets/Entry-Sidebar-Logo.svg";
+import ReadOnly from "../../assets/lock.svg";
+//TODO:  need to update the CTA button
+import Copy from "../../assets/Copy.svg";
+//TODO:  need to update the RC modal
+import RawConfigModal from "../../components/ViewRawConfig/RawConfigModal";
 
 const EntrySidebarExtension = () => {
+  const appConfig = useAppConfig();
+
+  const [isRawConfigModalOpen, setRawConfigModalOpen] = useState(false);
+
+  const handleViewRawConfig = () => {
+    setRawConfigModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setRawConfigModalOpen(false);
+  };
+
+  const sampleAppConfig = appConfig?.appConfigData || "";
+
   return (
-    <div className="entry-sidebar">
-      <div className="entry-sidebar-container">
-        <div className="entry-sidebar-icon">
-          <img src={Icon} alt="icon" />
-        </div>
-        <div className="app-component-content">
-          <h4>{localeTexts.SidebarWidget.title}</h4>
-          <p>{parse(localeTexts.SidebarWidget.body)}</p>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={localeTexts.SidebarWidget.button.url}>
-            {localeTexts.SidebarWidget.button.text}
-          </a>
+    <div className="layout-container">
+      <div className="ui-location-wrapper">
+        <div className="ui-location">
+          <div className="ui-container">
+            <div className="logo-container">
+              <img src={Icon} alt="Logo" />
+              <p>Sidebar Widget</p>
+            </div>
+            <div className="config-container">
+              <div className="label-container">
+                <p className="label">Sample App Configuration</p>
+                <p className="info">(read only)</p>
+              </div>
+              <div className="input-wrapper">
+                <div className="input-container">
+                  <p className="config-value">{sampleAppConfig}</p>
+                  <img src={ReadOnly} alt="ReadOnlyLogo" />
+                </div>
+
+                <img src={Copy} alt="Show-Json-CTA" className="show-json-cta" onClick={handleViewRawConfig} />
+                {isRawConfigModalOpen && <RawConfigModal config={appConfig!} onClose={handleCloseModal} />}
+              </div>
+            </div>
+            <div className="location-description">
+              <p className="location-description-text">{parse(localeTexts.SidebarWidget.body)}</p>
+              <a target="_blank" rel="noreferrer" href={localeTexts.SidebarWidget.button.url}>
+                <span className="location-description-link">{localeTexts.SidebarWidget.button.text}</span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
