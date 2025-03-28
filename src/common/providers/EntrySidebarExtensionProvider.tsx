65510daec2583b4cfd3c06496d7c3d995e99f242
MaitriGurey1 <maitri.gurey@contentstack.com>
2025-03-28 14:15:07 +0530

    fix: eslint errors

--- src/common/providers/EntrySidebarExtensionProvider.tsx
+++ src/common/providers/EntrySidebarExtensionProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { isEmpty, isNull } from "lodash";
 
 import { useAppLocation } from "../hooks/useAppLocation";
